b084997fac67adb3175d0e7c11c78f2ab8b82aa6
Dennis Won <jhwon0820@gmail.com>
2023-10-11 08:45:42 -0700

    chore: rename to account kit boilerplate

--- android/app/src/main/java/com/accountkitboilerplate/MainActivity.java
+++ android/app/src/main/java/com/accountkitboilerplate/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rntypescriptboilerplate;
+package com.AccountKitBoilerplate;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -23,7 +23,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RNTypescriptBoilerplate";
+    return "AccountKitBoilerplate";
   }
 
   /**
